b7f6c5bcefa1b34919ab041acda0ea408e19141b
Aqua200 <chinquepapa@gmail.com>
2024-12-11 17:34:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const rl = createInterface(process.stdin, process.stdout);
 const app = express();
 const port = process.env.PORT || 8080;
 
-say('Sisked\nBot', {
+say('Apeachy', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta'],
